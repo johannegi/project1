b56a8f240bb0f37701f00c7a0f361f37275d3ad5
thorfinnurp <thorfinnur16@ru.is>
2018-09-14 15:05:10 +0000

    flott

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+let customData = require('./data-1.json');
+//var obj = JSON.parse(customData);
+//console.log(obj));
+//let parsedData = require(JSON.parse('./data-1.json');
+//console.log(parsedData);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -9,14 +15,17 @@ const styles = StyleSheet.create({
   },
 });
 
+
 export default class App extends React.Component {
   render() {
     return (
+      
       <View style={styles.container}>
         <Text>Hello Swag World</Text>
+        
+        <Text>{JSON.stringify(customData)} </Text>
       </View>
     );
   }
 }
-
 

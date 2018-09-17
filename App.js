c0c1dbc1d243259442debd755ef21bebddf2c6d2
thorfinnurp <thorfinnur16@ru.is>
2018-09-17 13:07:06 +0000

    Fallegri kodi

--- App.js
+++ App.js
@@ -1,31 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, AppRegistry, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-var customData = require('./data-1.json');
+let customData = require('./data-1.json');
+let avatar = customData.map(item => item.avatar);
 
-//var data = customData.map(function(item) {
- // return {
-  //  key: item.name.first_name,
-  //  key2: item.name.last_name,
-//  };
-//});
-
-var firstName = customData.map(item => item.name.first_name);
-var lastName = customData.map(item => item.name.last_name);
-var homeAddress = customData.map(item => item.home.address);
-var homeEmail = customData.map(item => item.home.email);
-var homePhone = customData.map(item => item.home.phone_number);
-var avatar = customData.map(item => item.avatar);
-var workAddress = customData.map(item => item.work.address);
-var workEmail = customData.map(item => item.work.email);
-var workPhone = customData.map(item => item.work.phone_numnber);
-var workCompany = customData.map(item => item.work.company);
-var workDepartment = customData.map(item => item.work.department);
-var workJob = customData.map(item => item.work.job_title);
-//var obj = JSON.parse(customData);
-//console.log(obj));
-//let parsedData = require(JSON.parse('./data-1.json');
-//console.log(parsedData);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -57,15 +35,13 @@ export default class App extends React.Component {
       <View style={styles.container}>
        <Image
           style={{width: 400, height: 250}}
-          source={{uri: avatar[0]}}
+          source={{uri:  avatar[0]}}
         />
-        <Text>{firstName[0]} {lastName[0]}</Text>
+        <Text>{customData.map(item => item.name.first_name)} {customData.map(item => item.name.last_name)}</Text>
         <Button title = "Show work info" onPress = {this.addCounter}></Button>
-        
-        
-        <Text>{homeAddress[0]}</Text>
-        <Text>{homeEmail[0]}</Text>
-        <Text>{homePhone[0]}</Text>
+        <Text>{customData.map(item => item.home.address)}</Text>
+        <Text>{customData.map(item => item.home.email)}</Text>
+        <Text>{customData.map(item => item.home.phone_number)}</Text>
         
         
         
@@ -77,20 +53,18 @@ export default class App extends React.Component {
         <View style={styles.container}>
         <Image
           style={{width: 400, height: 250}}
-          source={{uri: avatar[0]}}
+          source={{uri: avatar[0]}} 
         />
-        <Text>{firstName[0]} {lastName[0]}</Text>
+        <Text>{customData.map(item => item.name.first_name)} {customData.map(item => item.name.last_name)}</Text>
         <Button title = "Show home info" onPress = {this.addCounter}></Button>
-        <Text>{workAddress[0]}</Text>
-        <Text>{workEmail[0]}</Text>
-        <Text>{workPhone[0]}</Text>
-        <Text>{workCompany[0]}</Text>
-        <Text>{workDepartment[0] , <Text>{workJob[0]}</Text>}</Text>
+        <Text>{customData.map(item => item.work.address)}</Text>
+        <Text>{customData.map(item => item.work.email)}</Text>
+        <Text>{customData.map(item => item.work.phone_numnber)}</Text>
+        <Text>{customData.map(item => item.work.company)}</Text>
+        <Text>{customData.map(item => item.work.department)} {customData.map(item => item.work.job_title)} </Text>
         </View>
       )
     }
     
     ;}
-      
-    
-  }
+}

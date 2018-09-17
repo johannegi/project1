f954f8973444c993453f32b9aa1668bb01006e54
thorfinnurp <thorfinnur16@ru.is>
2018-09-17 14:17:42 +0000

    CSS hlutir

--- App.js
+++ App.js
@@ -10,6 +10,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
+  },
+  outerContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 40,
+  },
+  baseText: {
+    fontSize: 20,
+    paddingTop: 5,
+  },
+  baseText2: {
+    width: '100%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 2.5,
+
+  },
+  button2: {
+    fontSize: 20,
+    padding: 10,
+    borderTopWidth: 30,
   },
 });
 
@@ -30,38 +51,47 @@ export default class App extends React.Component {
     if(this.state.counter % 2 == 0)
     {
     return (
-     
-      
-      <View style={styles.container}>
-       <Image
-          style={{width: 400, height: 250}}
-          source={{uri:  avatar[0]}}
-        />
-        <Text>{customData.map(item => item.name.first_name)} {customData.map(item => item.name.last_name)}</Text>
-        <Button title = "Show work info" onPress = {this.addCounter}></Button>
-        <Text>{customData.map(item => item.home.address)}</Text>
-        <Text>{customData.map(item => item.home.email)}</Text>
-        <Text>{customData.map(item => item.home.phone_number)}</Text>
-        
-        
-        
+      <View style={styles.outerContainer}>
+        <View style={styles.container}>
+          <Image
+            style={{width: 400, height: 250 }}
+            source={{uri: avatar[0]}}
+          />
+          <Text style = {styles.baseText}>{customData.map(item => item.name.first_name)} {customData.map(item => item.name.last_name)}</Text>
+
+          <Button style = {styles.button2} title = "Show work info" onPress = {this.addCounter}></Button>
+          <Text style = {styles.baseText}>{customData.map(item => item.home.address)}</Text>
+          <Text style = {styles.baseText2}> </Text>
+          <Text style = {styles.baseText}>{customData.map(item => item.home.email)}</Text>
+          <Text style = {styles.baseText2}> </Text>
+          <Text style = {styles.baseText}>{customData.map(item => item.home.phone_number)}</Text>
+          <Text style = {styles.baseText2}> </Text>
+            
+        </View>
       </View>
     )
     }
     else{
       return(
-        <View style={styles.container}>
-        <Image
-          style={{width: 400, height: 250}}
-          source={{uri: avatar[0]}} 
-        />
-        <Text>{customData.map(item => item.name.first_name)} {customData.map(item => item.name.last_name)}</Text>
-        <Button title = "Show home info" onPress = {this.addCounter}></Button>
-        <Text>{customData.map(item => item.work.address)}</Text>
-        <Text>{customData.map(item => item.work.email)}</Text>
-        <Text>{customData.map(item => item.work.phone_numnber)}</Text>
-        <Text>{customData.map(item => item.work.company)}</Text>
-        <Text>{customData.map(item => item.work.department)} {customData.map(item => item.work.job_title)} </Text>
+        <View style={styles.outerContainer}>
+          <View style={styles.container}>
+            <Image
+              style={{width: 400, height: 250}}
+              source={{uri: avatar[0]}} 
+            />
+            <Text style = {styles.baseText}>{customData.map(item => item.name.first_name)} {customData.map(item => item.name.last_name)}</Text>
+            <Button title = "Show home info" onPress = {this.addCounter}></Button>
+            <Text  style = {styles.baseText} >{customData.map(item => item.work.address)}</Text>
+            <Text style = {styles.baseText2}> </Text>
+            <Text  style = {styles.baseText}>{customData.map(item => item.work.email)}</Text>
+            <Text style = {styles.baseText2}> </Text>
+            <Text  style = {styles.baseText}>{customData.map(item => item.work.phone_number)}</Text>
+            <Text style = {styles.baseText2}> </Text>
+            <Text  style = {styles.baseText}>{customData.map(item => item.work.company)}</Text>
+            <Text style = {styles.baseText2}> </Text>
+            <Text style = {styles.baseText}>{customData.map(item => item.work.department)} {customData.map(item => item.work.job_title)} </Text>
+            <Text style = {styles.baseText2}> </Text>
+          </View>
         </View>
       )
     }

be4984e32441f885473df6eaddb090a5be8b8e39
johannegi <johannegi16@ru.is>
2018-09-18 13:30:36 +0000

    small fixes

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
-
-let customData = require("./data-1.json");
-
-let avatar = customData.map(item => item.avatar);
+//Get the data from the json file
+const customData = require("./data-1.json");
+//get the avatar from the json file with map
+const avatar = customData.map(item => item.avatar);
 
 const styles = StyleSheet.create({
   container: {
@@ -31,16 +31,16 @@ const styles = StyleSheet.create({
     paddingBottom: 15,
     marginLeft: 30,
     marginRight: 30,
-    backgroundColor: "#00BCD4",
+    backgroundColor: '#00BCD4',
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: "#fff"
+    borderColor: '#fff'
   },
   image: {
     borderWidth: 1,
     width: 300,
     height: 300,
-    borderColor: "black"
+    borderColor: 'black'
   }
 });
 
